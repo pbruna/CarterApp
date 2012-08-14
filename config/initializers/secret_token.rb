1a15aba4d6d360d5bad46dce75df6e1253fe6fe7
root <root@vagrant-centos-6.localdomain>
2012-08-14 09:55:46 -0400

    Primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carterapp::Application.config.secret_token = 'e34ee1165b9985695772bfbcce516743fc5230321a23b8b1dae6f5bd215fc5d047d398b0186cc25fc76043b4cc23819854b43c483d275f158cdb430eb2ba121b'
