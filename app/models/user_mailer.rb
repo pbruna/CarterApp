4d03bd77456b55279daafb8f9456ee7c935fa651
Patricio Bruna <pbruna@gmail.com>
2012-10-11 15:52:28 -0300

    Managing users and accounts ready

--- app/models/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default :from => "from@example.com"
-  
-  def welcome_email(user)
-    mail(:to => user.email, :subject => "Bienvenido a Carter")
-  end
-  
-end
