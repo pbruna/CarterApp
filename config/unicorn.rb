8390d4814a9e286fdd0fb928ee811edf5391124b
Patricio Bruna <pbruna@gmail.com>
2012-10-29 16:29:52 -0300

    Added Capistrano support

--- config/unicorn.rb
+++
@@ -1,9 +0,0 @@
-root_path = "/home/carter/App/CarterApp"
-worker_processes 4
-working_directory root_path
-listen 8080, :tcp_nopush => true
-pid "#{root_path}/tmp/unicorn.pid"
-stderr_path "#{root_path}/log/unicorn_error.log"
-stdout_path "#{root_path}/log/unicorn.log"
-timeout 30
-preload_app true
